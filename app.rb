f8402b41454f9179af40b6a4bda374ac1bcaf447
Daniel <danieleduardocald@gmail.com>
2021-01-11 14:50:03 -0500

    commit tree 386b17b41ee81922e764bb93dbf2eef861248809

--- app.rb
+++ app.rb
@@ -1,6 +1,23 @@
 class App < Sinatra::Base
 
-  # Define your GET '/' route below and respond with "Hello, World!"
+ get '/' do 
+    # Define your GET '/' route below and respond with "Hello, World!"
+"Hello, World!"
+ end
 
+ get '/name' do 
+  
+"My name is Corralero"
+end
+
+get '/hometown' do 
+  
+  "My hometown is Magajual"
+  end
+
+get '/favorite-song' do 
+  
+  "My favorite song is Festival en guarare"
+  end
 
 end
